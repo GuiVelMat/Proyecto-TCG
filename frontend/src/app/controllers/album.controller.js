6c1dc6ea1cb15abd10f4d912d5e47f341708f5c5
Guido <gvm0898@gmail.com>
2024-10-18 17:54:41 +0200

    render cards logic

--- frontend/src/app/controllers/album.controller.js
+++ frontend/src/app/controllers/album.controller.js
@@ -4,7 +4,8 @@ const loadAlbum = async () => {
     try {
         const cards = await CardService.getCardList();
         console.log(cards);
-        // Render the cards in the UI
+
+        return cards;
     } catch (error) {
         console.error('Error loading cards:', error);
     }
@@ -20,5 +21,26 @@ const loadRandomCard = async () => {
     }
 }
 
-loadAlbum();
-// loadRandomCard();
+const renderCards = (cards) => {
+    const cardGrid = document.querySelector('.card-grid');
+    cardGrid.innerHTML = ''; // Clear any existing cards
+
+    cards.forEach(card => {
+        const url = `http://localhost/Proyecto-TCG/frontend/src/assets/`
+        // console.log(card.image);
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+
+        const imgDiv = document.createElement('div');
+        imgDiv.innerHTML = `<img src="${url}${card.image}" alt="${card.name}">`;
+        cardElement.appendChild(imgDiv);
+
+        const nameElement = document.createElement('p');
+        nameElement.textContent = card.name;
+        cardElement.appendChild(nameElement);
+
+        cardGrid.appendChild(cardElement);
+    });
+}
+
+loadAlbum().then(cards => renderCards(cards));
