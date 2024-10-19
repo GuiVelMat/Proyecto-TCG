dc235ba298431c644d483a670615574af9c16ffd
Guido <gvm0898@gmail.com>
2024-10-19 16:09:40 +0200

    album with swal2 to add cards into deck

--- frontend/src/app/controllers/album.controller.js
+++ frontend/src/app/controllers/album.controller.js
@@ -28,8 +28,6 @@ const loadAlbumUser = async () => {
             }
         });
 
-        console.log(cardCount);
-
         return { cards, cardCount };
     } catch (error) {
         console.error('Error loading cards:', error);
@@ -46,17 +44,20 @@ const loadRandomCard = async () => {
     }
 }
 
-const renderCards = (cards, userCards, cardCount) => {
+const renderCardsAlbum = (cards, userCards, cardCount) => {
     const cardGrid = document.querySelector('.card-grid');
     cardGrid.innerHTML = '';
 
     // Mapeo de cartas del usuario para fácil búsqueda, sólo necesitamos el nombre para hacer el check
     const userCardMap = new Map(userCards.map(card => [card.name]));
+    const cardQuantity = [];
+
 
     // Crear todas las cartas del album
     cards.forEach(card => {
         const urlImg = `${window.location.origin}/src/assets/${card.image}`;
         const count = cardCount[card.name] || 0;
+        cardQuantity.push({ name: card.name, quantity: count });
 
         const renderedCard = document.createElement('div');
         renderedCard.innerHTML = `
@@ -101,17 +102,58 @@ const renderCards = (cards, userCards, cardCount) => {
 
         cardGrid.appendChild(renderedCard);
     });
+
+    attachCardClickEvents(cardQuantity);
+
+    // hacemos return por si queremos usar el objeto de cartas y sus cantidades
+    return cardQuantity;
 }
 
-const init = async () => {
+const attachCardClickEvents = (cardQuantity) => {
+    document.querySelectorAll('.card-container').forEach((container, index) => {
+        container.addEventListener('click', () => {
+            const cardName = container.querySelector('.card-name').textContent;
+
+            // Access the card and its quantity using the current index
+            if (cardQuantity[index].name === cardName && cardQuantity[index].quantity > 0) {
+                // check if card is Mana
+                if (cardName === "Basic mana" || cardName === "Super mana" || cardName === "Ultra mana") {
+                    Swal.fire({
+                        title: `Add ${cardName} to deck?`,
+                        showCancelButton: true,
+                        confirmButtonText: 'Confirm',
+                        cancelButtonText: 'Cancel',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            // document.querySelector('.deck-power').innerHTML = `Deck power ${cardQuantity[index].quantity} / 12`;
+                        }
+                    });
+                } else { // if card is not mana
+                    Swal.fire({
+                        title: `Set ${cardName} as active card?`,
+                        showCancelButton: true,
+                        confirmButtonText: 'Confirm',
+                        cancelButtonText: 'Cancel',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            // document.querySelector('.deck-power').innerHTML = `Deck power 1 / 12`;
+                        }
+                    });
+                }
+            }
+        });
+    });
+}
+
+
+const onInit = async () => {
     try {
         const allCards = await loadAlbum(); // Todas las cartas
         const userCards = await loadAlbumUser(); // Cartas del usuario
-        // console.log(userCards);
-        renderCards(allCards, userCards.cards, userCards.cardCount);
+        renderCardsAlbum(allCards, userCards.cards, userCards.cardCount);
     } catch (error) {
         console.error('Error initializing album:', error);
     }
 }
 
-init();
+onInit();
